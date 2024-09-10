22e705c14d0ebee389081d343cfe27c7b2444f78
jamKell <jak9425@nyu.edu>
2024-09-09 22:41:19 -0400

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -6,14 +6,14 @@ def welcome_assignment_answers(question):
     #Students do not have to follow the skeleton for this assignment.
     #Another way to implement is using a "case" statements similar to C.
     if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "Yes"
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "Yes"
     else: 
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
         ### maybe put something here to flag an issue and catch errors
-        answer = "Maybe"
+        answer = "maybe"
     return(answer)
 # Complete all the questions.
 
