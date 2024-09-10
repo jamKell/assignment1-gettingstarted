abc707f6143656ae14f2dc6af1e127cffdc7d17f
jamKell <jak9425@nyu.edu>
2024-09-09 23:05:02 -0400

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -8,15 +8,12 @@ def welcome_assignment_answers(question):
     if question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "Yes"
+        answer = "yes"
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
         answer = int(3)
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "yes"
-    elif question == "Are encoding and encryption the same? - Yes/No":
-        answer = "no"
+
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-          answer = int(7)
+          answer = "7"
 
     else: 
         ### you should understand why this else case should be included
